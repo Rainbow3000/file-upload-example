6729295d2b82010d2f92bf1907fe12ed114a8a1e
thinhnd <thinhnd@rabiloo.com>
2025-02-13 11:13:49 +0700

    feat: add server static module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FileUploadModule } from './file-upload/file-upload.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [FileUploadModule],
+  imports: [
+    FileUploadModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'), // Trỏ đến thư mục chứa ảnh
+      serveRoot: '/uploads', // URL để truy cập ảnh
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
